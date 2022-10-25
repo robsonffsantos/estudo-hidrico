bf651d42f67a42a31ea63f38293fcf0ec79ffcd5
Robson Fernando <robson.2501@hotmail.com>
2022-10-24 22:23:48 -0300

    style: main height changed

--- src/pages/home/style.tsx
+++ src/pages/home/style.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const MainContainer = styled.div`
     display: flex;
     flex-direction: column;
-    height: 90vh;
+    height: 98vh;
     justify-content: space-between;
     align-items: center;
 `
