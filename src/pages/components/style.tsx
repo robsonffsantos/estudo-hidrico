3ca9e97350a20482af3286fbeba7f94ca528120e
Robson Fernando <robson.2501@hotmail.com>
2022-10-24 22:02:10 -0300

    feat: header

--- src/pages/components/style.tsx
+++ src/pages/components/style.tsx
@@ -4,6 +4,8 @@ import background from '../../assets/dropsBackground.png'
 export const HeaderBackground = styled.img`
     background-image: url(${background});
     background-size: contain;
-    width: 100vw;
+    width: 100%;
     height: 10vh;
+    display: flex;
+    justify-content: center;
 `
