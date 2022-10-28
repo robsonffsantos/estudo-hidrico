a5d36aac6a0bc80450441b4c0e3dad4781c1d18f
Robson Fernando <robson.2501@hotmail.com>
2022-10-28 19:42:56 -0300

    feat: added all menu items

--- src/pages/content/menu/index.tsx
+++ src/pages/content/menu/index.tsx
@@ -1,15 +1,26 @@
-import { MainOption, MainTitle, MenuContainer, SecondaryTitle } from "./style"
+import { MainOption, MainTitle, MenuContainer, Option, SecondaryTitle, ThreeOption } from "./style"
 
 
 const Menu = () => {
     return (
         <MenuContainer>
             <MainTitle>Menu</MainTitle>
-            <SecondaryTitle>Área de Estudo</SecondaryTitle>
-            <SecondaryTitle>Produtividade Hídrica</SecondaryTitle>
-            <SecondaryTitle>-fazendas-</SecondaryTitle>
-            <SecondaryTitle>Escassez Hídrica</SecondaryTitle>
-            <SecondaryTitle>Quem Somos</SecondaryTitle>
+                <SecondaryTitle>Área de Estudo</SecondaryTitle>
+                    <MainOption>Bacia Hidrográfica</MainOption>
+                <SecondaryTitle>Produtividade Hídrica<br/>
+                    -fazendas-</SecondaryTitle>
+                    <ThreeOption>
+                        Produção de leite
+                            <Option>A pasto</Option>
+                            <Option>Semi-confinado</Option>
+                            <Option>Confinado</Option>
+                    </ThreeOption>
+                    <MainOption>Aves de corte</MainOption>
+                    <MainOption>Suínos</MainOption>                   
+                <SecondaryTitle>Escassez Hídrica</SecondaryTitle>
+                    <MainOption>Impactos Escassez</MainOption>
+                <SecondaryTitle>Quem Somos</SecondaryTitle>
+                    <MainOption>Autores</MainOption>
         </MenuContainer>
     )
 }
--- src/pages/content/menu/style.tsx
+++ src/pages/content/menu/style.tsx
@@ -4,20 +4,49 @@ export const MenuContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin: 1vmax 0.5vmax;
+    justify-content: space-between;
+    margin: 1vmax 0.5vmax 1vmax 0.5vmax;
     font-family: Arial, Helvetica, sans-serif;
     color: #404040;
+    height: 95%;
 `
 
 export const MainTitle = styled.div`
-    font-size: 18px;
+    font-size: 22px;
     font-weight: bolder;
 `
 
 export const SecondaryTitle = styled.div`
-    font-size: 16px;
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
 `
 
 export const MainOption = styled.div`
     border: 1px solid #ced4da;
+    width: 75%;
+    height: 5%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 14px;
+`
+
+export const ThreeOption = styled.div`
+    border: 1px solid #ced4da;
+    width: 75%;
+    height: 25%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    font-size: 14px;
+`
+
+export const Option = styled.div`
+    border: 1px solid #ced4da;
+    width: 70%;
+    height: 20%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `
